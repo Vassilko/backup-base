c5593dde8b0063df72cfc07bd5177988e9a6d25a
Vasiliy <vassio@ukr.net>
2014-08-01 17:48:02 +0300

    made some changes

--- backup-dropbox/src/main/java/ua/od/vassio/backup/dropbox/DropBoxResourceOpener.java
+++ backup-dropbox/src/main/java/ua/od/vassio/backup/dropbox/DropBoxResourceOpener.java
@@ -1,10 +1,133 @@
 package ua.od.vassio.backup.dropbox;
 
+import com.dropbox.core.*;
+import liquibase.resource.ResourceAccessor;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  * User: vassio
  * Date: 27.07.14
  * Time: 11:37
  */
-public class DropBoxResourceOpener {
+public class DropBoxResourceOpener implements ResourceAccessor {
+
+    private String app_key ;
+    private String app_secret;
+    private String app_name;
+
+    public DropBoxResourceOpener(String app_name) {
+        this.app_name = app_name;
+    }
+
+    public DropBoxResourceOpener(String app_key, String app_secret, String app_name) {
+        this.app_key = app_key;
+        this.app_secret = app_secret;
+        this.app_name = app_name;
+        init();
+    }
+
+    DbxClient client;
+
+    public void init() {
+        DbxAppInfo appInfo = new DbxAppInfo(app_key, app_secret);
+        DbxRequestConfig config = new DbxRequestConfig(
+                "DropBoxResourceOpener/1.0", Locale.getDefault().toString());
+        DbxWebAuth webAuth = new DbxWebAuth(config, appInfo,);
+        webAuth.start();
+            String authorizeUrl = webAuth.;
+        try{
+            String authorizationCode;
+            DbxAuthFinish authFinish = webAuth.finish(authorizationCode);
+            String accessToken = authFinish.accessToken;
+            client = new DbxClient(config, accessToken);
+            System.out.println("Linked account: " + client.getAccountInfo().displayName);
+        } catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+
+
+
+    public void setApp_key(String app_key) {
+        this.app_key = app_key;
+    }
+
+    public void setApp_secret(String app_secret) {
+        this.app_secret = app_secret;
+    }
+
+    public void setAuthorizationCode(String authorizationCode) {
+        this.authorizationCode = authorizationCode;
+    }
+
+    @Override
+    public Set<InputStream> getResourcesAsStream(String path) throws IOException {
+        if (client==null){
+            throw new IllegalAccessError("DropBoxResourceOpener must be init first");
+        }
+
+        Set<InputStream> inputStreams=new HashSet<InputStream>();
+
+        try{
+            DbxEntry.WithChildren listing = client.getMetadataWithChildren(path);
+            for (DbxEntry child : listing.children) {
+                  if (child.isFile()){
+                      inputStreams.add(getInputStreamFromFile(child));
+                  }
+             }
+        } catch (DbxException ex){
+            throw new RuntimeException(ex);
+        }
+
+        return inputStreams;
+    }
+
+    private InputStream getInputStreamFromFile(DbxEntry file) throws IOException, DbxException {
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+         try{
+             DbxEntry.File downloadedFile = client.getFile(file.path, null,
+                     byteArrayOutputStream);
+             System.out.println("Metadata: " + downloadedFile.toString());
+            return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+         } finally {
+             byteArrayOutputStream.close();
+         }
+
+    }
+
+    @Override
+    public Set<String> list(String relativeTo, String path, boolean includeFiles, boolean includeDirectories, boolean recursive) throws IOException {
+        if (client==null){
+            throw new IllegalAccessError("DropBoxResourceOpener must be init first");
+        }
+        Set<String> list=new HashSet<>() ;
+        try{
+            DbxEntry.WithChildren listing = client.getMetadataWithChildren(path);
+            for (DbxEntry child : listing.children) {
+                if (child.isFile()){
+                    list.add(child.path);
+                }
+            }
+        } catch (DbxException ex){
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+
+    @Override
+    public ClassLoader toClassLoader() {
+        return this.getClass().getClassLoader();
+    }
+
+
 }
--- backup-dropbox/src/test/java/ua/od/vassio/backup/dropbox/DropBoxResourceOpenerTest.java
+++ backup-dropbox/src/test/java/ua/od/vassio/backup/dropbox/DropBoxResourceOpenerTest.java
@@ -1,5 +1,14 @@
 package ua.od.vassio.backup.dropbox;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
 /**
  * Created with IntelliJ IDEA.
  * User: vassio
@@ -7,4 +16,22 @@ package ua.od.vassio.backup.dropbox;
  * Time: 12:31
  */
 public class DropBoxResourceOpenerTest {
+    DropBoxResourceOpener dropBoxResourceOpener=new DropBoxResourceOpener();
+           final String APP_KEY = "kuebk1pz38bbg75";
+        final String APP_SECRET = "dp3u216fym0wsgz";
+
+    @BeforeMethod
+    public void initDropBoxResourceOpener(){
+        dropBoxResourceOpener.setApp_key(APP_KEY);
+        dropBoxResourceOpener.setApp_secret(APP_SECRET);
+        dropBoxResourceOpener.setAuthorizationCode("");
+        dropBoxResourceOpener.init();
+    }
+    @Test
+    public void testReadFile() throws IOException {
+        Set<String> strings= dropBoxResourceOpener.list("", "", false, false, false);
+         assertNotNull(strings);
+        assertEquals(strings.size(),1);
+        assertEquals(strings.iterator().next(),"");
+    }
 }
