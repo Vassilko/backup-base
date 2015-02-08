822d6e427a0967fcab4f8d8c56eca74e18f9c008
Vasiliy <vassio@ukr.net>
2015-02-08 22:47:32 +0200

    fix connection leak

--- backup-dropbox/src/main/java/ua/od/vassio/backup/dropbox/main/DropboxMain.java
+++ backup-dropbox/src/main/java/ua/od/vassio/backup/dropbox/main/DropboxMain.java
@@ -21,85 +21,95 @@ import java.sql.SQLException;
  * Time: 15:39
  */
 public class DropboxMain {
-    private static Logger logger= LoggerFactory.getLogger(DropboxMain.class);
+    private static Logger logger = LoggerFactory.getLogger(DropboxMain.class);
 
-    private static String APP_NAME=DropboxMain.class.getSimpleName();
+    private static String APP_NAME = DropboxMain.class.getSimpleName();
 
     public static void main(String args[]) throws Exception {
         LogBackInitializer.initialization(DropboxMain.class.getClassLoader().getResourceAsStream("logback.xml"));
-        CommandLine commandLine=ArgumentsHelper.getCommandLine(args,DropboxArgumentAction.class);
+        CommandLine commandLine = ArgumentsHelper.getCommandLine(args, DropboxArgumentAction.class);
         logger.info("read command line parameters");
-        String connectionString= ArgumentsHelper.getArgument(args,DropboxArgumentAction.CONNECTION_STRING,commandLine,String.class);
-        String accessToken= ArgumentsHelper.getArgument(args,DropboxArgumentAction.ACCESS_TOKEN,commandLine,String.class);
-        String appName=ArgumentsHelper.getArgument(args,DropboxArgumentAction.APP_NAME,commandLine,String.class);
-        String dropBoxActionString=ArgumentsHelper.getArgument(args,DropboxArgumentAction.ACTION,commandLine,String.class);
-        DropBoxAction dropBoxAction=EnumUtils.getEnum(DropBoxAction.class,dropBoxActionString);
-        String fileName=ArgumentsHelper.getArgument(args,DropboxArgumentAction.FILENAME,commandLine,String.class);
+        String connectionString = ArgumentsHelper.getArgument(args, DropboxArgumentAction.CONNECTION_STRING, commandLine, String.class);
+        String accessToken = ArgumentsHelper.getArgument(args, DropboxArgumentAction.ACCESS_TOKEN, commandLine, String.class);
+        String appName = ArgumentsHelper.getArgument(args, DropboxArgumentAction.APP_NAME, commandLine, String.class);
+        String dropBoxActionString = ArgumentsHelper.getArgument(args, DropboxArgumentAction.ACTION, commandLine, String.class);
+        DropBoxAction dropBoxAction = EnumUtils.getEnum(DropBoxAction.class, dropBoxActionString);
+        String fileName = ArgumentsHelper.getArgument(args, DropboxArgumentAction.FILENAME, commandLine, String.class);
 
         logger.info("validate parameters");
-        if (StringUtils.isEmpty(connectionString)){
+        if (StringUtils.isEmpty(connectionString)) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(DropboxMain.class.getSimpleName(), ArgumentsHelper.buildOptions(DropboxArgumentAction.class));
-           throw new IllegalArgumentException("connectionString is empty");
+            throw new IllegalArgumentException("connectionString is empty");
         }
-        if (StringUtils.isEmpty(accessToken)){
+        if (StringUtils.isEmpty(accessToken)) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(DropboxMain.class.getSimpleName(), ArgumentsHelper.buildOptions(DropboxArgumentAction.class));
             throw new IllegalArgumentException("accessToken is empty");
         }
-        if (dropBoxAction==null){
+        if (dropBoxAction == null) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(DropboxMain.class.getSimpleName(), ArgumentsHelper.buildOptions(DropboxArgumentAction.class));
             throw new IllegalArgumentException("Action is empty");
         }
-        if (dropBoxAction==DropBoxAction.UPDATE ){
-            if (StringUtils.isEmpty(fileName)){
+        if (dropBoxAction == DropBoxAction.UPDATE) {
+            if (StringUtils.isEmpty(fileName)) {
                 HelpFormatter formatter = new HelpFormatter();
                 formatter.printHelp(DropboxMain.class.getSimpleName(), ArgumentsHelper.buildOptions(DropboxArgumentAction.class));
                 throw new IllegalArgumentException("fileName is empty");
             }
         }
-        if (StringUtils.isEmpty(appName)){
+        if (StringUtils.isEmpty(appName)) {
             logger.warn("Applicatioon Name is Empty use " + APP_NAME);
-            appName= APP_NAME;
+            appName = APP_NAME;
         }
 
-        String jdbcDriver=ArgumentsHelper.getArgument(args,DropboxArgumentAction.DRIVER,commandLine,String.class);
-         if (StringUtils.isNotEmpty(jdbcDriver)){
-             Class.forName(jdbcDriver);
-         }
+        String jdbcDriver = ArgumentsHelper.getArgument(args, DropboxArgumentAction.DRIVER, commandLine, String.class);
+        if (StringUtils.isNotEmpty(jdbcDriver)) {
+            Class.forName(jdbcDriver);
+        }
         //getConnection
         logger.info("getConnection");
-        Connection connection=getConnection(connectionString);
-        //init logic
-        logger.info("init logic");
-        DropboxLiquibaseStructureSaver dropboxLiquibaseStructureSaver=new DropboxLiquibaseStructureSaver(connection,appName,accessToken);
-        dropboxLiquibaseStructureSaver.init();
+        Connection connection = getConnection(connectionString);
+        try {
+            //init logic
+            logger.info("init logic");
+            DropboxLiquibaseStructureSaver dropboxLiquibaseStructureSaver = new DropboxLiquibaseStructureSaver(connection, appName, accessToken);
+            dropboxLiquibaseStructureSaver.init();
 
-        if (ArgumentsHelper.getArgumentWithOutArgument(args,DropboxArgumentAction.DROP,commandLine)&&dropBoxAction==DropBoxAction.UPDATE){
-            dropboxLiquibaseStructureSaver.dropAll();
+            if (ArgumentsHelper.getArgumentWithOutArgument(args, DropboxArgumentAction.DROP, commandLine) && dropBoxAction == DropBoxAction.UPDATE) {
+                dropboxLiquibaseStructureSaver.dropAll();
+            }
+
+            //start logic
+            logger.info("start logic " + dropBoxAction);
+            if (dropBoxAction == DropBoxAction.SAVE) {
+                dropboxLiquibaseStructureSaver.save();
+            }
+            if (dropBoxAction == DropBoxAction.DROP) {
+                dropboxLiquibaseStructureSaver.dropAll();
+            }
+            if (dropBoxAction == DropBoxAction.CLEAR) {
+                dropboxLiquibaseStructureSaver.dropAll();
+            }
+            if (dropBoxAction == DropBoxAction.UPDATE) {
+                dropboxLiquibaseStructureSaver.update(fileName);
+            }
+            logger.info("finish Successfully");
+        } finally {
+            if (!connection.isClosed()) {
+                connection.commit();
+                connection.close();
+            }
         }
 
-        //start logic
-        logger.info("start logic "+dropBoxAction);
-        if (dropBoxAction==DropBoxAction.SAVE){
-            dropboxLiquibaseStructureSaver.save();
-        }
-        if (dropBoxAction==DropBoxAction.DROP){
-            dropboxLiquibaseStructureSaver.dropAll();
-        }
-        if (dropBoxAction==DropBoxAction.CLEAR){
-            dropboxLiquibaseStructureSaver.dropAll();
-        }
-        if (dropBoxAction==DropBoxAction.UPDATE){
-            dropboxLiquibaseStructureSaver.update(fileName);
-        }
-        logger.info("finish Successfully");
     }
 
     private static Connection getConnection(String connectionString) throws SQLException {
-        Connection connection= DriverManager
+        Connection connection = DriverManager
                 .getConnection(connectionString);
         return connection;
-    };
+    }
+
+    ;
 }
